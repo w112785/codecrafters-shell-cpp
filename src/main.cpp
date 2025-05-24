38ec3ccde5a4f0f2d8da0c00c57b611b76534afd
William E McCormick <w112785@gmail.com>
2025-05-23 21:09:17 -0400

    type push

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,16 @@ int main() {
     std::getline(std::cin, input);
     //if (input == "exit")
     if (input.starts_with("exit"))    {      return 0;    }
-    else if (input.find("echo") != std::string::npos)
-    {
+    else if (input.starts_with("echo")){
       std::cout << input.substr(5) << std::endl;
     }
+    else if (input.starts_with("type"))
+      if (input.substr(0, 9) == "type echo" || input.substr(0, 9) == "type exit" || input.substr(0, 9) == "type type") {
+        std::cout << input.substr(5) + " is a shell builtin" << std::endl;
+      }
+      else {
+        std::cout << input.substr(5) + ": not found" << std::endl;
+      }
     else{
     std::cout << input << ": command not found" << std::endl;
     }
