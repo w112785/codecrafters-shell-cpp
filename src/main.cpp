96a30cd36d267c2c80441cd0a5174ba04d554a8f
William E McCormick <w112785@gmail.com>
2025-05-24 23:20:44 -0400

    type filename working

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
+#include <filesystem>
+
+std::vector<std::string> builtins;
+
+std::vector<std::string> SplitString(const std::string& str, char delimiter = ' ') {
+  std::istringstream iss(str);
+  std::vector<std::string> tokens;
+  std::string token;
+  while (std::getline(iss, token, delimiter)) {
+    tokens.push_back(token);
+  }
+  return tokens;
+}
+
+bool SearchDirectorys(const std::vector<std::string> &paths, std::string& file){
+  
+  return false;
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -7,26 +27,43 @@ int main() {
   std::cerr << std::unitbuf;
 
   std::string input;
+  std::vector<std::string> command;
 
   while (true)
   {
     // Uncomment this block to pass the first stage
     std::cout << "$ ";
     std::getline(std::cin, input);
+
+    command = SplitString(input);
     //if (input == "exit")
-    if (input.starts_with("exit"))    {      return 0;    }
-    else if (input.starts_with("echo")){
+    if (command[0] == "exit") { return 0; }
+    else if (command[0] == "echo"){
       std::cout << input.substr(5) << std::endl;
     }
-    else if (input.starts_with("type"))
-      if (input.substr(0, 9) == "type echo" || input.substr(0, 9) == "type exit" || input.substr(0, 9) == "type type") {
-        std::cout << input.substr(5) + " is a shell builtin" << std::endl;
+    else if (command[0] == "type") {
+      if (command.size() > 1) {
+        if (command[1] == "echo" || command[1] == "exit" || command[1] == "type") {
+          std::cout << command[1] << " is a shell builtin" << std::endl;
+        }
+        else {
+          std::vector<std::string> path;
+          // becuase I run windows and Windows and linux run different path seperators
+#ifdef _WIN32
+          path = SplitString(std::getenv("PATH"), ';');
+#elif defined(_POSIX_VERSION)
+          path = SplitString(std::getenv("PATH"), ':');
+#endif
+
+          std::cout << command[1] << ": not found" << std::endl;
+        }
       }
-      else {
-        std::cout << input.substr(5) + ": not found" << std::endl;
+      else{
+          std::cout << "Usage: type [name]" << std::endl;
       }
+    }
     else{
-    std::cout << input << ": command not found" << std::endl;
+      std::cout << input << ": command not found" << std::endl;
     }
   }
 }
