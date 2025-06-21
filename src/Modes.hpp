9026963595b355d08f6365c577c8138a555102e3
William E McCormick <w112785@gmail.com>
2025-06-20 22:47:45 -0400

    double quote 1

--- src/Modes.hpp
+++ src/Modes.hpp
@@ -1,4 +1,4 @@
-enum class Modes{
+enum class Modes{ // Used to work on Modes for the Split string and handle quotes. This is going to get a little complex.
     NormalMode,
     SingleQuote,
     DoubleQuote
--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,44 @@
 
 #include "Modes.hpp"
 
-std::vector<std::string> SplitString(const std::string& str, char delimiter, bool searchForQuotes = false) {
+std::vector<std::string> SplitString(const std::string& str, char delimiter, bool tokenize = false) {
   std::istringstream iss(str);
   std::vector<std::string> tokens;
   std::string token;
-  if (searchForQuotes){
-    bool inSingleQuotes = false;
-
+  Modes mode = Modes::NormalMode;
+  bool escaped = false;
+  if (tokenize) {
     for (size_t i = 0; i < str.length(); i++) {
       char c = str[i];
+      if (escaped) { // previous char was '\'
+        token += c;
+        escaped = false;
+        continue;
+      }
 
-      if (c == '\'') {
-        if (!inSingleQuotes) {
-          inSingleQuotes = true;
-        } else {
-          inSingleQuotes = false;
+      switch (mode) {
+      case Modes::NormalMode:
+        if (c == '\'') {
+          mode = Modes::SingleQuote;
         }
-      } else if (c == delimiter && !inSingleQuotes) {
+        if (c == '"') {
+          mode = Modes::DoubleQuote;
+        }
+        break;
+
+      case Modes::SingleQuote:
+        if (c == '\'') {
+          mode = Modes::NormalMode;
+        }
+        break;
+
+      case Modes::DoubleQuote:
+        if (c == '"' && !escaped) {
+          mode = Modes::NormalMode;
+        }
+        break;
+      }
+      if (c == delimiter && mode == Modes::NormalMode) {
         if (!token.empty()) {
           tokens.push_back(token);
           token.clear();
@@ -34,6 +55,7 @@ std::vector<std::string> SplitString(const std::string& str, char delimiter, boo
     if (!token.empty()) {
       tokens.push_back(token);
       token.clear();
+
     }
   } else {
     while (std::getline(iss, token, delimiter)) {
@@ -60,7 +82,7 @@ bool SearchDirectorys(const std::vector<std::string>& paths, const std::string& 
 
 // This was added to keep things better organized. 
 // As I continue this challenge I am finding I need to keep this code scalable.
-struct BuiltinCommand { 
+struct BuiltinCommand {
   std::string name;
   std::string usage;
   bool (*exeCommand)(std::vector<std::string> cmd);
@@ -70,15 +92,15 @@ std::vector<BuiltinCommand> builtins;
 std::vector<std::string> path;
 std::string input;
 
-bool pwd(std::vector<std::string> cmd){
+bool pwd(std::vector<std::string> cmd) {
   std::cout << std::filesystem::current_path().string() << "\n";
   return true;
 }
 
-bool CmdType(std::vector<std::string> cmd){
+bool CmdType(std::vector<std::string> cmd) {
   if (cmd.size() > 1) {
-    for (BuiltinCommand builtin: builtins){
-      if (cmd[1] == builtin.name){
+    for (BuiltinCommand builtin : builtins) {
+      if (cmd[1] == builtin.name) {
         std::cout << cmd[1] << " is a shell builtin" << std::endl;
         return true;
       }
@@ -86,43 +108,39 @@ bool CmdType(std::vector<std::string> cmd){
     std::string typeresult;
     if (!SearchDirectorys(path, cmd[1], typeresult)) {
       std::cout << cmd[1] << ": not found" << std::endl;
-    }
-    else {
+    } else {
       std::cout << cmd[1] << " is " << typeresult << std::endl;
     }
     return true;
-  }
-  else {
+  } else {
     return false;
   }
 }
 
-bool CmdEcho(std::vector<std::string> cmd){
-  if (cmd.size() <= 1){
+bool CmdEcho(std::vector<std::string> cmd) {
+  if (cmd.size() <= 1) {
     return false;
   }
-  for (int x = 1; x <= (cmd.size() - 1); x++){
+  for (int x = 1; x <= (cmd.size() - 1); x++) {
     std::cout << cmd[x] << " ";
   }
   std::cout << std::endl;
   return true;
 }
 
-bool CmdCd(std::vector<std::string> cmd){
-  if (std::filesystem::exists(cmd[1])){
+bool CmdCd(std::vector<std::string> cmd) {
+  if (std::filesystem::exists(cmd[1])) {
     std::filesystem::current_path(cmd[1]);
-  }
-  else if (cmd[1] == "~"){
+  } else if (cmd[1] == "~") {
     std::filesystem::current_path(std::getenv("HOME"));
-  }
-  else{
+  } else {
     std::cout << "cd: " << cmd[1] << ": No such file or directory" << std::endl;
   }
   return true;
 }
 
-bool CmdExit(std::vector<std::string> cmd){return false;} //was created but after rethough shouild never be called.
-                                                          // keeping to create a pointer for the exit call
+bool CmdExit(std::vector<std::string> cmd) { return false; } //was created but after rethough shouild never be called.
+// keeping to create a pointer for the exit call
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -138,11 +156,11 @@ int main() {
   path = SplitString(std::getenv("PATH"), ':');
 #endif
 
-  builtins.push_back({"exit", " ", *CmdExit});
-  builtins.push_back({"echo", "Usage: echo [text or numbers to echo to the screen] \nFuture will implement a way to get Enviromnet Variables from this command \nEG. $PATH", *CmdEcho});
-  builtins.push_back({"pwd", " ", *pwd});
-  builtins.push_back({"type", "Usage: type [name]", *CmdType });
-  builtins.push_back({"cd", " ", *CmdCd});
+  builtins.push_back({ "exit", " ", *CmdExit });
+  builtins.push_back({ "echo", "Usage: echo [text or numbers to echo to the screen] \nFuture will implement a way to get Enviromnet Variables from this command \nEG. $PATH", *CmdEcho });
+  builtins.push_back({ "pwd", " ", *pwd });
+  builtins.push_back({ "type", "Usage: type [name]", *CmdType });
+  builtins.push_back({ "cd", " ", *CmdCd });
 
   while (true) {
     std::cout << "$ ";
@@ -150,11 +168,25 @@ int main() {
 
     command_found = false;
     command = SplitString(input, ' ', true);
+    if (command.empty()) {
+      continue;
+    }
 
-    for (BuiltinCommand builtincmd: builtins) {
+    for (BuiltinCommand builtincmd : builtins) {
       if (command[0] == builtincmd.name) {
-        if (builtincmd.name == "exit"){
-          return 0;
+        if (builtincmd.name == "exit") {
+          if (command.size() > 1) { // edited so I can actually pass the exit challenge. even though it only does 0 for the test.
+            try {
+              int exitCode = std::stoi(command[1]);
+              if (exitCode >= 0 && exitCode <= 255) {
+                return exitCode;
+              }
+            } catch (std::invalid_argument) {
+              return 0;
+            } catch (std::out_of_range) {
+              return 1;
+            }
+          }
         }
         if (!builtincmd.exeCommand(command)) {
           std::cout << builtincmd.usage << std::endl;
@@ -162,7 +194,7 @@ int main() {
         command_found = true;
       }
     }
-    if (!command_found){
+    if (!command_found) {
       std::string exeCommand;
       if (SearchDirectorys(path, command[0], exeCommand)) {
         system(input.c_str());
@@ -170,43 +202,5 @@ int main() {
       }
       std::cout << input << ": command not found" << std::endl;
     }
-    /*
-    //if (input == "exit")
-    if (command[0] == "exit") { return 0; }
-    else if (command[0] == "echo"){
-      std::cout << input.substr(5) << std::endl;
-    }
-    else if (command[0] == "pwd"){
-      std::cout << std::filesystem::current_path().string() << "\n";
-    }
-    else if (command[0] == "type") {
-      if (command.size() > 1) {
-        if (command[1] == "echo" || command[1] == "exit" || command[1] == "type" || command[1] == "pwd") {
-          std::cout << command[1] << " is a shell builtin" << std::endl;
-        }
-        else {
-          std::string typeresult;
-          if (!SearchDirectorys(path, command[1], typeresult)) {
-            std::cout << command[1] << ": not found" << std::endl;
-          }
-          else {
-            std::cout << command[1] << " is " << typeresult << "\n";
-          }
-        }
-      }
-      else{
-          std::cout << "Usage: type [name]" << std::endl;
-      }
-    }
-    else{
-      std::string exeCommand;
-      if (SearchDirectorys(path, command[0], exeCommand)){
-        system(input.c_str());
-        continue;
-      }
-
-      std::cout << input << ": command not found" << std::endl;
-    }
-    */
   }
 }
