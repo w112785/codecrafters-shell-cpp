837b13fd6471ceefc292736ba1a3138129a9b705
William E McCormick <w112785@gmail.com>
2025-07-15 00:22:55 -0400

    Added Solution files to project, started seperating fuctions

---
+++ src/tools.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <filesystem>
+#include <format>
+
+std::vector<std::string> SplitString(const std::string& str, char delimiter, bool tokenize = false);
